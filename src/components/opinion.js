3ac4eae6ae1a639d575c4a7787449c2cc05178e8
Ollie Rogers <ollie.rogers@rationetwork.co.uk>
2018-05-23 22:29:26 +0100

    add a TODO

--- src/components/opinion.js
+++ src/components/opinion.js
@@ -1,3 +1,7 @@
+// TODO
+// - Add some more specific comments depending on gust and wind direction.
+// - Save wind data to chart a history of conditions.
+
 import React, { Component } from 'react';
 
 const Opinion = ({ content }) => {
