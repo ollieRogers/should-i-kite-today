3508f0ba52486b393eb90d879e57aba7e618e0dd
Ollie Rogers <ollie.rogers@rationetwork.co.uk>
2018-10-11 15:56:29 +0100

    add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-63178035-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+      },
+    }
+],
 }
