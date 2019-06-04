a96ae2cecbafc95875f99e834189f383fb892672
ollie <ollie@ratio.co.uk>
2019-06-04 08:38:26 +0100

    add new key

--- src/components/map.js
+++ src/components/map.js
@@ -1,14 +1,10 @@
-import React, { useContext } from "react"
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-} from "react-google-maps"
-import { GlobalContext } from "../context/contextProvider"
+import React, { useContext } from "react";
+import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
+import { GlobalContext } from "../context/contextProvider";
 
 const WindCompass = withScriptjs(
   withGoogleMap(() => {
-    const [weatherData] = useContext(GlobalContext)
+    const [weatherData] = useContext(GlobalContext);
     return (
       <GoogleMap
         defaultZoom={13}
@@ -19,28 +15,28 @@ const WindCompass = withScriptjs(
           <div
             className="compass-arrow"
             style={{
-              transform: `rotate(${weatherData.bearing}deg)`,
+              transform: `rotate(${weatherData.bearing}deg)`
             }}
           />
         )}
       </GoogleMap>
-    )
+    );
   })
-)
+);
 
 const mapStyle = {
   height: "100%",
   position: "absolute",
-  width: "100%",
-}
+  width: "100%"
+};
 
 const Map = () => (
   <WindCompass
-    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDOxa8-lFMbda75ruLL45CqSLIqevsxDiU"
+    googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyAYaYCWfLUeLYa6b_45D0X5vJHljwZmGYs`}
     loadingElement={<div style={mapStyle} />}
     containerElement={<div style={mapStyle} />}
     mapElement={<div style={mapStyle} />}
   />
-)
+);
 
-export default Map
+export default Map;
